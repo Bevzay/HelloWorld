82f707478a1d4e499e8f062eeebd3fcfb18a7545
Beverly Ann Gupit <Bevzay@users.noreply.github.com>
2019-10-08 22:47:30 +1300

    Print variable

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
         System.out.println("Hello World!");
 
         int myFirstNumber = 5;  // declaration statement
+        System.out.println(myFirstNumber);
     }
 }
